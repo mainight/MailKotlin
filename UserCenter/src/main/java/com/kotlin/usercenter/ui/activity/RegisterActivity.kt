6b9c915355a8df39ccd5317c2fc0e1de643b72fe
mainight <39176507+mainight@users.noreply.github.com>
2020-11-01 19:00:56 +0800

    Test

--- UserCenter/src/main/java/com/kotlin/usercenter/ui/activity/RegisterActivity.kt
+++ UserCenter/src/main/java/com/kotlin/usercenter/ui/activity/RegisterActivity.kt
@@ -5,19 +5,14 @@ import android.os.Bundle
 import android.widget.Toast
 import com.kotlin.usercenter.R
 import kotlinx.android.synthetic.main.activity_register.*
-import org.jetbrains.anko.intentFor
-import org.jetbrains.anko.toast
-import java.awt.font.TextAttribute
+import org.jetbrains.anko.startActivity
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-
         mRegisterBtn.setOnClickListener {
-            //startActivity(intentFor<TestActivity>("id" to 5))
-            startActivity(intentFor<TestActivity>("id" to 5))
-            toast("注册")
+            startActivity<TestActivity>("id" to 10)
         }
     }
 }
--- UserCenter/src/main/java/com/kotlin/usercenter/ui/activity/TestActivity.kt
+++ UserCenter/src/main/java/com/kotlin/usercenter/ui/activity/TestActivity.kt
@@ -3,13 +3,31 @@ package com.kotlin.usercenter.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.kotlin.usercenter.R
-import org.jetbrains.anko.toast
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
 
 class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test)
-         toast("${intent.getIntExtra("id",-1)}")
+        verticalLayout {
+            padding=30
+            editText{
+                hint="name"
+                textSize=24f
+            }
+            editText{
+               hint="Pwd"
+                textSize=24f
+            }
+            button{
+               text="Test"
+                onClick{
+                    toast("${intent.getIntExtra("id",-1)}")
+                }
 
+            }
+
+        }
+         toast("${intent.getIntExtra("id",-1)}")
     }
 }
