f4874f4203c1fe7bb95bb4bd8237109e13975a99
mainight <39176507+mainight@users.noreply.github.com>
2020-11-01 17:04:07 +0800

    anko

--- UserCenter/src/main/java/com/kotlin/usercenter/ui/activity/RegisterActivity.kt
+++ UserCenter/src/main/java/com/kotlin/usercenter/ui/activity/RegisterActivity.kt
@@ -5,13 +5,19 @@ import android.os.Bundle
 import android.widget.Toast
 import com.kotlin.usercenter.R
 import kotlinx.android.synthetic.main.activity_register.*
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.toast
+import java.awt.font.TextAttribute
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
         mRegisterBtn.setOnClickListener {
-            Toast.makeText(this, "注册", Toast.LENGTH_SHORT).show()
+            //startActivity(intentFor<TestActivity>("id" to 5))
+            startActivity(intentFor<TestActivity>("id" to 5))
+            toast("注册")
         }
     }
 }
